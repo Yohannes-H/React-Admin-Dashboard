cc2f62b78bd049f52c10d0b5634a0524dd47babe
Yohannes Hailu <yohanneshdev21@gmail.com>
2022-09-05 02:35:12 +0300

    New page for products and users is updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Single from "./pages/single/Single";
 import New from "./pages/new/New";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { productInputs, userInputs } from "./formSource";
 function App() {
   return (
     <div className="App">
@@ -16,12 +17,15 @@ function App() {
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route path="new" element={<New inputs={userInputs} />} />
             </Route>
             <Route path="products">
               <Route index element={<List />} />
               <Route path=":productId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route
+                path="new"
+                element={<New inputs={productInputs} title="Add new Product" />}
+              />
             </Route>
           </Route>
         </Routes>
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,7 +1,58 @@
 import React from "react";
 import "./new.scss";
-function New() {
-  return <div>New</div>;
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
+function New({ inputs, title = null }) {
+  const [file, setFile] = React.useState("");
+  return (
+    <div className="new">
+      <Sidebar />
+
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>{title || "Add New User"}</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src={
+                file
+                  ? URL.createObjectURL(file)
+                  : "https://icons.veryicon.com/png/o/application/designe-editing/add-image-1.png"
+              }
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadIcon className="icon" />
+                </label>
+                <input
+                  type="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  id="file"
+                  style={{ display: "none" }}
+                />
+              </div>
+
+              {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label htmlFor="">{input.label}</label>
+                  <input type={input.type} placeholder={input.placeholder} />
+                </div>
+              ))}
+
+              <button>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default New;
